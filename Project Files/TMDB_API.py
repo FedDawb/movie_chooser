f679b98f1f751b5fcb6d96985ab75fa68a6e4e3d
TasianJoseph <tasianpjoseph@gmail.com>
2024-11-25 19:49:15 +0000

    Creating files relevant to API tasks with little notes to loosely explain what each file should house and be used for

---
+++ Project Files/TMDB_API.py
@@ -0,0 +1,8 @@
+# This file will be used to house the API calls and the relevant urls to manage:
+# searching for films
+# returning details
+# finding matches etc
+
+"""
+
+"""
---
+++ Project Files/config.py
@@ -0,0 +1,4 @@
+#  This config will house:
+#  TMDB API keys
+#  Connection to our Database(s)
+#  Connection to our Front End
