423d753d07309d97ce724b27fd168b0f567807a5
TasianJoseph <tasianpjoseph@gmail.com>
2024-11-25 21:27:00 +0000

    Following Flask Login tutorial - wrote basic flask script in order to create user login

--- Project Files/app.py
+++ Project Files/app.py
@@ -1,3 +1,9 @@
+from flask import Flask, render_template
+# importing packages:
+# importing flask class from flask package
+# render template for testing
+
+
 #  This file will house Flask API code, manage routing and integrate with front-end
 """
 route ideas
@@ -16,3 +22,23 @@ route ideas
 5. enables user to add or remove a film on the favourites list
 / faves / edit (add/delete a fave film)
 """
+
+# asking flask to use this file to run the request server side
+app = Flask(__name__)
+
+# app routing:
+# the ROOT ADDRESS for our application is "/" our first function "home()" will manage the logic for the landing page
+# using the app route decorator "app.route" binds the function and its logic to this root url
+# running on port address http://127.0.0.1:5000/
+
+
+@app.route("/")
+def home():
+    return "This thing is on!"
+
+
+# telling the script to run if running this file and using the debugger to ensure it runs correctly and if not it will
+# tell us immediately
+
+if __name__ == "__main__":
+    app.run(debug=True)
