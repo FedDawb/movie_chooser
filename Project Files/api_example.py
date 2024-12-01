38b792d3bc5f9660bead4a76f4aab15db4248c13
Bexidecimal <rebeccadsouthcombe@gmail.com>
2024-12-01 20:09:38 +0000

    Creation of API endpoints, creation of logic to search for a film and return recommendations based on that.
    Created a requirements.txt and updated the .env file to not upload to github.

--- Project Files/api_example.py
+++ Project Files/api_example.py
@@ -2,7 +2,7 @@ class Film:
     def fake_search_api(self):
         results = [
             {
-                "poster_path": "/IfB9hy4JH1eH6HEfIgIGORXi5h.jpg",
+                "poster_path": "/lXoATMtTK3ZsICCvTtXygASHZd9.jpg",
                 "adult": False,
                 "overview": "Jack Reacher must uncover the truth behind a major government conspiracy in order to clear his name. On the run as a fugitive from the law, Reacher uncovers a potential secret from his past that could change his life forever.",
                 "release_date": "2016-10-19",
@@ -24,7 +24,7 @@ class Film:
                 "vote_average": 4.19
             },
             {
-                "poster_path": "/another-file.jpg",
+                "poster_path": "/IfB9hy4JH1eH6HEfIgIGORXi5h.jpg",
                 "adult": False,
                 "overview": "Jack Reacher must do stuff",
                 "release_date": "2018-10-01",
@@ -46,7 +46,7 @@ class Film:
                 "vote_average": 3.29
             },
             {
-                "poster_path": "/Holiday-Fever.jpg",
+                "poster_path": "/IfB9hy4JH1eH6HEfIgIGORXi5h.jpg",
                 "adult": False,
                 "overview": "Jack Reacher goes on holiday",
                 "release_date": "2020-07-25",
@@ -68,7 +68,7 @@ class Film:
                 "vote_average": 1.23
             },
             {
-                "poster_path": "/another-file.jpg",
+                "poster_path": "/IfB9hy4JH1eH6HEfIgIGORXi5h.jpg",
                 "adult": False,
                 "overview": "Jack Reacher must do stuff",
                 "release_date": "2018-10-01",
---
+++ Project Files/endpoints/tmdb.py
@@ -0,0 +1,133 @@
+class Genre:
+
+    def __init__ (self):
+        self.genres()
+        self.genre_id_to_name = {a["id"]:a["name"] for a in self._genres}
+        self.genre_name_to_id = dict((v,k) for k,v in self.genre_id_to_name.items())
+
+    def genres(self):
+        self._genres = [
+            {
+                "id": 28,
+                "name": "Action"
+            },
+            {
+                "id": 12,
+                "name": "Adventure"
+            },
+            {
+                "id": 16,
+                "name": "Animation"
+            },
+            {
+                "id": 35,
+                "name": "Comedy"
+            },
+            {
+                "id": 80,
+                "name": "Crime"
+            },
+            {
+                "id": 99,
+                "name": "Documentary"
+            },
+            {
+                "id": 18,
+                "name": "Drama"
+            },
+            {
+                "id": 10751,
+                "name": "Family"
+            },
+            {
+                "id": 14,
+                "name": "Fantasy"
+            },
+            {
+                "id": 36,
+                "name": "History"
+            },
+            {
+                "id": 27,
+                "name": "Horror"
+            },
+            {
+                "id": 10402,
+                "name": "Music"
+            },
+            {
+                "id": 9648,
+                "name": "Mystery"
+            },
+            {
+                "id": 10749,
+                "name": "Romance"
+            },
+            {
+                "id": 878,
+                "name": "Science Fiction"
+            },
+            {
+                "id": 10770,
+                "name": "TV Movie"
+            },
+            {
+                "id": 53,
+                "name": "Thriller"
+            },
+            {
+                "id": 10752,
+                "name": "War"
+            },
+            {
+                "id": 37,
+                "name": "Western"
+            }
+        ]
+
+
+class AgeRating:
+
+    def __init__ (self):
+        self.certificate_rating()
+        self.rating_list = sorted(self._ratings, key=lambda x: x['order'])
+
+    def certificate_rating(self):
+
+        self._ratings = [
+      {
+        "certification": "15",
+        "meaning": "Only those over 15 years are admitted. Nobody younger than 15 can rent or buy a 15-rated VHS, DVD, Blu-ray Disc, UMD or game, or watch a film in the cinema with this rating. Films under this category can contain adult themes, hard drugs, frequent strong language and limited use of very strong language, strong violence and strong sex references, and nudity without graphic detail. Sexual activity may be portrayed but without any strong detail. Sexual violence may be shown if discreet and justified by context.",
+        "order": 5
+      },
+      {
+        "certification": "R18",
+        "meaning": "Can only be shown at licensed adult cinemas or sold at licensed sex shops, and only to adults, those aged 18 or over. Films under this category are always hard-core pornography, defined as material intended for sexual stimulation and containing clear images of real sexual activity, strong fetish material, explicit animated images, or sight of certain acts such as triple simultaneous penetration and snowballing. There remains a range of material that is often cut from the R18 rating: strong images of injury in BDSM or spanking works, urolagnia, scenes suggesting incest even if staged, references to underage sex or childhood sexual development and aggressive behaviour such as hair-pulling or spitting on a performer are not permitted. More cuts are demanded in this category than any other category.",
+        "order": 7
+      },
+      {
+        "certification": "PG",
+        "meaning": "All ages admitted, but certain scenes may be unsuitable for young children. May contain mild language and sex/drugs references. May contain moderate violence if justified by context (e.g. fantasy).",
+        "order": 2
+      },
+      {
+        "certification": "12A",
+        "meaning": "Films under this category are considered to be unsuitable for very young people. Those aged under 12 years are only admitted if accompanied by an adult, aged at least 18 years, at all times during the motion picture. However, it is generally not recommended that children under 12 years should watch the film. Films under this category can contain mature themes, discrimination, soft drugs, moderate swear words, infrequent strong language and moderate violence, sex references and nudity. Sexual activity may be briefly and discreetly portrayed. Sexual violence may be implied or briefly indicated.",
+        "order": 3
+      },
+      {
+        "certification": "U",
+        "meaning": "All ages admitted, there is nothing unsuitable for children.",
+        "order": 1
+      },
+      {
+        "certification": "18",
+        "meaning": "Only adults are admitted. Nobody younger than 18 can rent or buy an 18-rated VHS, DVD, Blu-ray Disc, UMD or game, or watch a film in the cinema with this rating. Films under this category do not have limitation on the bad language that is used. Hard drugs are generally allowed, and explicit sex references along with detailed sexual activity are also allowed. Scenes of strong real sex may be permitted if justified by the context. Very strong, gory, and/or sadistic violence is usually permitted. Strong sexual violence is permitted unless it is eroticised or excessively graphic.",
+        "order": 6
+      },
+      {
+        "certification": "12",
+        "meaning": "Home media only since 2002. 12A-rated films are usually given a 12 certificate for the VHS/DVD version unless extra material has been added that requires a higher rating. Nobody younger than 12 can rent or buy a 12-rated VHS, DVD, Blu-ray Disc, UMD or game. The content guidelines are identical to those used for the 12A certificate.",
+        "order": 4
+      }
+    ]
