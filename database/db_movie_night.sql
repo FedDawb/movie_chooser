e341a097ecb62b0f9367e219112b328a1f151991
AmyCSimpson <amy.c.simpson@icloud.com>
2024-12-07 19:51:43 +0000

    Amended database to add favourites table for functionality.

--- database/db_movie_night.sql
+++ database/db_movie_night.sql
@@ -4,14 +4,18 @@ USE db_movie_night;
 
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    preferences JSON -- Stores genres, actors, etc.
+    username VARCHAR(250) NOT NULL UNIQUE,
+    preferences JSON, -- Stores genres, actors, etc.
+    age INT NOT NULL,
+    email VARCHAR(250) NOT NULL UNIQUE,
+    password_hash VARCHAR(250) NOT NULL
 );
 
+-- api_id has been used for the movie identifier
 CREATE TABLE User_Items (
     user_item_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    api_id VARCHAR(255) NOT NULL, -- Unique ID from the API
+    api_id VARCHAR(250) NOT NULL, -- Unique ID from the API
     watched_date DATE,
     liked BOOLEAN,
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
@@ -20,16 +24,17 @@ CREATE TABLE User_Items (
 CREATE TABLE Blocked_Items (
     blocked_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    api_id VARCHAR(255) NOT NULL, -- Unique ID from the API
+    api_id VARCHAR(250) NOT NULL, -- Unique ID from the API
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
-ALTER TABLE Users
-ADD COLUMN age INT NOT NULL;
-
-ALTER TABLE Users
-ADD COLUMN email VARCHAR(255) NOT NULL UNIQUE,
-ADD COLUMN password_hash VARCHAR(255) NOT NULL;
+CREATE TABLE Favourites (
+    favourite_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    api_id VARCHAR(250) NOT NULL, -- Unique ID from the API
+    added_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Automatically stores the date when added
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
+);
 
 
 
