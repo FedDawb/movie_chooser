cb8adbc56331617e2e57ee97663c5a439a38c88d
Emily <emily@kt3.co.uk>
2024-12-08 17:37:21 +0000

    Front end update to redirect user post sign up

--- ProjectFiles/users.py
+++ ProjectFiles/users.py
@@ -42,18 +42,15 @@ class Users:
         return False  # the user entered a password that doesn't match the hashed one
 
 
-<<<<<<< Updated upstream
     def check_email(email):
         valid_email = r"\b[A-Za-z0-9._%+-] +@[A-Z|a-z]{2,}\b"
         return re.match(valid_email, email)
 
 
 # creating an 18+ and <18 user subclass to filter what films are available to them based on their age at sign up
-=======
 def check_email(email):
     valid_email = r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
     return re.match(valid_email, email)
->>>>>>> Stashed changes
 
 
 # telling the subclasses that this should be implemented and raise an error if not
